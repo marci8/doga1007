b3aee785cd582d2096f64fc99f4f8f7a975f5651
gaspar.marcell <gaspar.marcell@28-DIAK13>
2024-10-07 14:48:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using doga1007;
+using System.Text;
+
+List<Triatlon> events = [];
+
+using StreamReader sr = new(
+    path: @"..\..\..\src\forras.txt",
+    encoding: Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream) events.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"{events.Count} darab versenyző fejezte be a versenyt");
+
+var tCnt = events.Count(e => e.Kategoria == "25-29");
+Console.WriteLine($"A 25-29 - es kategóriába tartozó versenyzők száma: {tCnt} darab");
+
---
+++ Triatlon.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doga1007
+{
+    internal class Triatlon
+    {
+        public string Nev { get; set; }
+        public int Year { get; set; }
+        public int Rajtszam { get; set; }
+        public string Neme {  get; set; }
+        public string Kategoria { get; set; }
+        public DateTime Uszas { get; set; }
+        public DateTime Elso { get; set; }
+        public DateTime Kerekpar { get; set; }
+        public DateTime Masodik { get; set; }
+        public DateTime Futas { get; set; }
+
+        public override string ToString()
+        {
+            return
+            $"\tNev: {Nev}\n" +
+            $"\tSzületési év: {Year}\n" +
+            $"\tRajtszám: {Rajtszam}\n" +
+            $"\tNem: {Neme}\n" +
+            $"\tKategória: {Kategoria}\n" +
+            $"\tÚszás ideje: {Uszas}\n" +
+            $"\tElső depóban töltött idő: {Elso}\n" +
+            $"\tKerékpározás ideje: {Kerekpar}\n" +
+            $"\tMásodik depóban töltött idő: {Masodik}\n" +
+            $"\tFutás ideje: {Futas}\n";
+        }
+
+        public Triatlon(string row)
+        {
+            var v = row.Split(';');
+            Nev = v[0];
+            Year = int.Parse(v[1]);
+            Rajtszam = int.Parse(v[2]);
+            Neme = v[3];
+            Kategoria = v[4];
+            Uszas = DateTime.Parse(v[5]);
+            Elso = DateTime.Parse(v[6]);
+            Kerekpar = DateTime.Parse(v[7]);
+            Masodik = DateTime.Parse(v[8]);
+            Futas = DateTime.Parse(v[9]);
+        }
+    }
+}
